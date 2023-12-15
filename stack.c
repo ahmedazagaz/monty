df2d3538f87a549f098669a99a4ff45de9f27e00
ahmedazagaz <contactwithazagaz@gmail.com>
2023-12-15 06:47:54 -0800

    up

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - Affiche l'élément en haut de la pile
+ * @head: Pointeur vers le sommet de la pile
+ * @counter: Numéro de ligne
+ * Retour: Aucune valeur de retour
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
